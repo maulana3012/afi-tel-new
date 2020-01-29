03264c58b9493564d3531b9d4b991c12bf2489e3
maulana3012 <muhamadmaulanarachman@gmail.com>
2020-01-29 14:06:04 +0700

    update

---
+++ language/es.js
@@ -0,0 +1,152 @@
+var Translations = {
+    "SkipBackButtonText": "Atrás",
+    "SkipForwardButtonText": "Avanzar",
+    "PlayPauseButtonText": "Reproducir/Pausar",
+    "VolumeButtonText": "Volumen",
+    "SettingsButtonText": "Ajustes",
+    "ResumeVideoPlayback": "Reanudar la reproducción de vídeo",
+
+    "QualityMenuText": "Calidad",
+    "QualityAutoText": "Auto",
+    "LanguageMenuText": "Idioma",
+    "CCMenuText": "Subtítulos",
+    "BackFromText": "Back from",
+    "OnText": "On",
+    "OffText": "Off",
+
+    "SocialIcon-facebook": "Facebook",
+    "SocialIcon-twitter": "Twitter",
+    "SocialIcon-googleplus": "Google+",
+    "SocialIcon-email": "Email",
+    "SocialIcon-link": "Website",
+    "CollapseText": "Colapso",
+    "ExpandText": "Expandir",
+
+    "PreparedForText": "Preparado para",
+
+    "CopyrightText": "Copyright " + Utils.getFullYear() + " Client Name",
+    "PoweredByFooterText": "Powered by",
+    "HelpFooterText": "Ayuda",
+    "PrivacyPolicyFooterText": "Política de privacidad",
+
+    /*
+     * See en.js for details
+     */
+    "CtaButtonSettings": {
+        "button1": {
+            "card": "7b480c70",
+            "label": "Botón 1",
+            "icon": "contact"
+        },
+        "button2": {
+            "card": "7b480c70",
+            "label": "Botón 2",
+            "icon": "website"
+        },
+        "button3": {
+            "card": "7b480c70",
+            "label": "Botón 3",
+            "icon": "email"
+        },
+        "button4": {
+            "card": "7b480c70",
+            "label": "Botón 4",
+            "icon": "save"
+        },
+        "button5": {
+            "url": "http://example.com/es",
+            "label": "Botón 5",
+            "icon": "share"
+        }
+    },
+
+    /*
+     * See en.js for details
+     */
+    "PromoSettings": {
+        "promo1": {
+            "trackingName": "promo1",
+            "url": "http://example.com/es",
+            "label": "Promo (Top)",
+            "desktopImage": "./images/promo_portrait.png",
+            "mobileImage": "./images/promo_landscape.png"
+        },
+        "promo2": {
+            "trackingName": "promo2",
+            "url": "http://example.com/es",
+            "label": "Promo (Bottom)",
+            "desktopImage": "./images/promo_portrait.png",
+            "mobileImage": "./images/promo_landscape.png"
+        }
+    },
+
+    /*
+     * See en.js for details
+     */
+    "ChapterSettings": [
+        {
+            "label": "<p>Welcome<br/>Speech</p>",
+            "states": [
+                {
+                    "cardId": "START",
+                    "width": 14
+                }
+            ]
+        },
+        {
+            "label": "<p>Thank You<br/>Speech</p>",
+            "states": [
+                {
+                    "cardId": "7ccfe36b",
+                    "width": 15
+                }
+            ]
+        },
+        {
+            "label": "<p>Verification<br/>Customer Data</p>",
+            "states": [
+                {
+                    "cardId": "a7e0f675",
+                    "width": 20
+                }
+            ]
+        },
+        {
+            "label": "<p>Product<br/>Info</p>",
+            "states": [
+                {
+                    "cardId": "50c10b64",
+                    "width": 12
+                }
+            ]
+        },
+        {
+            "label": "<p>Product<br/>Detail</p>",
+            "states": [
+                {
+                    "cardId": "5be243e3",
+                    "width": 13
+                }
+            ]
+        },
+        {
+            "label": "<p>Legal<br/>Statement</p>",
+            "states": [
+                {
+                    "cardId": "780b0f32",
+                    "width": 14
+                }
+            ]
+        },
+        {
+            "label": "<p>Close<br>Speech",
+            "states": [
+                {
+                    "cardId": "b91d1b8e",
+                    "width": 12
+                }
+            ]
+        }
+
+    ]
+}
